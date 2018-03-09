fafbb33760b29641bb3b80138ea0ea3a99170da5
Angel Sanadinov <angel.sanadinov@lunatech.com>
2018-03-09 16:23:26 +0100

    Add pippo dep

--- src/main/kotlin/bardo/Bardo.kt
+++ src/main/kotlin/bardo/Bardo.kt
@@ -1,5 +1,18 @@
 package bardo
 
+import ro.pippo.core.Pippo
+
 fun main(args: Array<String>) {
     println("hello world?")
+    val pippo = Pippo()
+
+    pippo.GET("/") {
+        routeContext -> routeContext.send("Hello World?!?!?!")
+    }
+
+    pippo.GET("/test") {
+        routeContext -> routeContext.send("test")
+    }
+
+    pippo.start()
 }
